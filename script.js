83994ad4ebf138f91735fe023724befe95e71c32
AbhiramNp <abhimysterio@gmail.com>
2022-07-31 23:38:57 +0530

    2 number operations working

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-// function add(a,b){
-//     return a+b;
-// }
-// function sub(x,y){
-//     return x-y;
-// }
-// function div(a,b){
-//     return a/b;
-// }
-// function multiply(a,b){
-//     return a*b;
-// };
-// add(3,2);
-// sub(3,2);
 
 let number = document.querySelectorAll(".btn-number");
 let operator = document.querySelectorAll(".btn-operator")
@@ -21,34 +7,74 @@ let display = document.querySelector(".display");
 display.innerText = 0;
 let firstNum = "";
 let secondNum = "";
-let operative = "";
+let op = "";
 
-
-function operate(firstNum,secondNum,operative){
+function operate (Num1,Num2,op){
 number.forEach(numbers =>{
     numbers.addEventListener('click',(e)=>{
-        numb = e.target.innerText;
-        num = parseInt(numb);
-        display.innerText = num;
-        console.log(num);
-        console.log(typeof num)
+        if(op === ""){
+        num1 = e.target.innerText;
+        Num1 = parseInt(num1);
+        display.innerText = Num1;
+        console.log(Num1);
+        console.log(typeof Num1)
+        }
+        else{
+            num2 = e.target.innerText;
+            Num2 = parseInt(num2);
+            display.innerText = Num2;
+            console.log(Num2);
+            console.log(typeof Num2)
+            }
 
-    })
+        }
+
+    )
 })
 
 operator.forEach(operators => {
 operators.addEventListener('click',(e) => {
-    op = e.target.innerText;
-    console.log(op);
-    console.log(typeof op)
+    if(e.target.innerText === '='){
+        switch(op){
+            case '+':
+                let plus = Num1 + Num2
+                display.innerText = plus;
+                console.log(plus);
+                console.log(typeof plus);
+                break;
+            case '-':
+                let minus = Num1 - Num2
+                display.innerText = minus;
+                console.log(minus);
+                console.log(typeof minus);
+                break;
+            case '*':
+                let multiple = Num1 * Num2
+                display.innerText = multiple;
+                console.log(multiple);
+                console.log(typeof multiple);
+                break;
+            case '/':
+                let divide = Num1 / Num2;
+                display.innerText = divide;
+                console.log(divide);
+                console.log(typeof divide);
+                break;
+    
+    
+            default:console.log("Value is default")
+               break;
+        }
+        }        
+    else
+    {
+        op = e.target.innerText;
+        console.log(op);
+        console.log(typeof op)
+        }
+    })
 })
     
-});
-}
+};
 
-
-
-// function operate(num1,num2,operator)
-// {
-
-// }
+operate(firstNum,secondNum,op);
