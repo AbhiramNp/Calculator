8ff30bdd9ce2e2409f584232993c3c50cfe44ad8
AbhiramNp <abhimysterio@gmail.com>
2022-08-04 23:37:51 +0530

    Still have works to do

--- script.js
+++ script.js
@@ -1,125 +1,158 @@
+let x;
+let y;
+let operator;
+let result;
 
-let number = document.querySelectorAll(".btn-number");
-let operator = document.querySelectorAll(".btn-operator")
-let clear = document.querySelector(".btn-clear")
-let dot = document.querySelector(".btn-float")
-let display = document.querySelector(".display");
-display.innerText = 0;
-let firstNum = "";
-let secondNum = "";
-let op = "";
-let numArray1 = [];
-let numArray2 = [];
+let math = {
+    x: "",
+    y: "",
+    operator: "",
+    firstNumArray: [],
+    secondNumArray: [],
+};
+
+const display = document.getElementById('display');
+const numberButtons = document.querySelectorAll('.numberButton');
+const operatorButtons = document.querySelectorAll('.operatorButton');  
+const equalButton = document.querySelector('.equalButton');
+const clearButton = document.querySelector('.clearButton');
+
+//sets the value of the operator
+
+operatorButtons.forEach((btn) => {
+    btn.addEventListener('click', function(e) {
+        console.log("operator button clicked")
+        math.operator = btn.value    
+        updateDisplay();
+        console.log(math)
+        //if (result)
 
 
-
-function operate (Num1,Num2,oper){
-number.forEach(numbers =>{
-    numbers.addEventListener('click',(e)=>{
+// will check that result is equal to a truthy value,
+// which means anything except 
+// false, 0, -0, 0n, "", null, undefined, and NaN.
+// once that condition is satisfied / passes,
+// then it will run the block, otherwise it won't
 
         
-        if(oper === "" ) {
-        num1 = e.target.innerText;
-        numArray1.push(num1);
-        console.log(numArray1);
-        if(numArray1.length <= 7){
-        Num1=numArray1.join("");
-        Num1=parseInt(Num1);
-        console.log(Num1);
-        display.innerText = Num1;
-        console.log(typeof Num1)
-        }
-        else{
-            alert("Maximum numbers");
-        }
-        }
-        else{
-            num2 = e.target.innerText;
-            numArray2.push(num2);
-            console.log(numArray2);
-            if(numArray2.length <= 7){
-            Num2=numArray2.join("");
-            Num2=parseInt(Num2);
-            console.log(Num2);
-            display.innerText = Num2;
-            console.log(typeof Num2)
-            }
-            else{
-                alert("Maximum numbers");
-            }
-            }
-
-        }
-
-    )
-})
-
-
-function sumesh(Num1,Num2,opt){
-operator.forEach(operators => {
-operators.addEventListener('click',(e) => {
-    if(e.target.innerText === '='){
-         
-        switch(oper){
-            case '+':
-                let plus = Num1 + Num2
-                display.innerText = plus;
-                console.log(plus);
-                console.log(typeof plus);
-                break;
-            case '-':
-                let minus = Num1 - Num2
-                display.innerText = minus;
-                console.log(minus);
-                console.log(typeof minus);
-                break;
-            case '*':
-                let multiple = Num1 * Num2
-                display.innerText = multiple;
-                console.log(multiple);
-                console.log(typeof multiple);
-                break;
-            case '/':
-                if(Num2 === 0){
-                    alert("This cannot happen")
-                    break;
-                }
-                let divide = Num1 / Num2;
-                if(Num1 < Num2){
-                divide = divide.toFixed(2);
-                display.innerText = divide;
-                }
-                display.innerText = divide;
-                console.log(divide);
-                console.log(typeof divide);
-                break;
-    
-    
-            default:console.log("Value is default")
-               break;
-        }
-        }        
-    else
-    {
-        oper = e.target.innerText;
-        console.log(oper);
-        console.log(typeof oper)
+        if (math.y) {               //if y exists and we push operator then x will be the answer and everything else will be emplty
+            math = {
+                x: result,
+                y: "",
+                operator: btn.value,
+                firstNumArray: [],
+                secondNumArray: [],
+            };
+            console.log(result);
+            display.textContent = result;
+            updateDisplay();
+            return result;
         }
     })
-})
-} 
-sumesh();
+});
 
-}
 
+        //set value of first and second number in relation to operator
+            numberButtons.forEach((button) => {
+            button.addEventListener('click', function(ev) {
+                console.log("Number button clicked")
+                if (math.operator) {
+                    math.secondNumArray.push(button.value);
+                    y = math.secondNumArray.join('');
+                    console.log(y);
+                    math.y = parseFloat(y);
+                    console.log(typeof math.y);
+        
+                    // math.y += button.value;
+                } else {
+                    math.firstNumArray.push(button.value);
+                    x = math.firstNumArray.join('');
+                    math.x = parseFloat(x);
+                    // math.x += button.value;
+                }
+                console.log(math)
+                updateDisplay();
+        
+        /*-------sets solution to previous problem as new first input-------*/
+                if (math.operator) {
+                    result = (operation(math.x, math.operator, math.y));
+                    console.log(" result = (operation(math.x, math.operator, math.y));")
+                }
+            });
+        });
+
+        
+      /*-------SHOULD solve the function-------*/
+      equalButton.addEventListener('click', function(e){
+        if (result)
+            math = {
+                x: result,
+                y: "",
+                operator: "",
+                firstNumArray: [],
+                secondNumArray: [],
+            };
+        display.textContent = result;
+        console.log("result is " + result)
+        console.log(math);
+      })
+      function updateDisplay() {
+        
+        display.textContent = `${math.x} ${math.operator} ${math.y}`;
+        console.log("Displaying");
+        
+    }
     
-operate(firstNum,secondNum,op);
+    
+    function add(x, y) {
+        console.log("Adding");
+        return x + y;
+    };
+    
+    function subtract (x, y) {
+        console.log("subtracting");
 
+        return x - y;
+    };
+    
+    function multiply(x, y) {
+        console.log("multiplying");
 
+        return x * y;
+    };
+    
+    function divide (x, y) {
+        console.log("dividing");
 
-
-
-clear.addEventListener('click',() => {
-    window.location.reload();
-})
+        if(y === 0 && x!== 0){
+            alert("NO DONT DO IT")
+        }
+        else{
+        return x / y;
+        }
+    };
+    
+    function operation(x, operator, y) {
+    
+        if (operator == "+") {
+            console.log("Addition operator check")
+            return add(x, y);
+        } else if (operator == "-") {
+            console.log("subtraction operator check")
+           return subtract(x, y);
+        } else if (operator == "*") {
+            console.log("multiplication operator check")
+            return multiply(x, y)
+        } else if (operator == "/") {
+            console.log("divison operator check")
+            return divide(x, y)
+        }
+          
+    
+    };
+    
+    clearButton.addEventListener('click',() => {
+        console.log("Clear Button Clicked")
+        window.location.reload();
+    })
 
